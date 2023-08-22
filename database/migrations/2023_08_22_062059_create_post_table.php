7372249b8376d81d61c1cfc74010652f78224ca6
strelok93 <strelok0493@gmail.com>
2023-08-22 11:22:50 +0500

    fa

---
+++ database/migrations/2023_08_22_062059_create_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->string('post_name');
+            $table->string('post_img');
+            $table->string('post_type');
+            $table->text('post_desc');
+            $table->bigInteger('counter');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+};
